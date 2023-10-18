91d230c18a0c1c313763c695edc63c9180c5d41f
Jaden-cho0115 <144252728+Jaden-cho0115@users.noreply.github.com>
2023-10-18 17:09:07 -0400

    Added changes to script.js

--- Exercies/Exercise 5/script.js
+++ Exercies/Exercise 5/script.js
@@ -6,6 +6,16 @@ const inputText = document.getElementById('inputText');
 
 const textLength = document.getElementById('text-length');
 
+const loopContainer = document.getElementById('loopContainer');
+
+const textToAdd = 'LOOP ';
+
+const increaseText = document.getElementById('increaseText');
+
+const square = document.getElementById('square');
+
+let fontSize = 16;
+
 circles.forEach((circle) => {
     circle.addEventListener('click', function () {
         const color = window.getComputedStyle(circle).backgroundColor;
@@ -13,7 +23,6 @@ circles.forEach((circle) => {
     });
 });
 
-const square = document.getElementById('square');
 document.addEventListener('mousemove', (e) => {
     const y = e.clientY;
     const color = `rgb(0, ${y % 255}, 0)`;
@@ -25,21 +34,16 @@ setInterval(function () {
     increaseText.style.fontSize = `${fontSize}px`;
 }, 1000);
 
-const loopContainer = document.getElementById('loopContainer');
-const textToAdd = 'LOOP ';
-for (let i = 0; i < 10; i++) {
-    const span = document.createElement('span');
-    span.textContent = textToAdd;
-    loopContainer.appendChild(span);
-}
-
-const increaseText = document.getElementById('increaseText');
-let fontSize = 16;
-
 document.getElementById('form').addEventListener('submit', function (e) {
     e.preventDefault();
     const inputLength = inputText.value.length;
     textLength.textContent = `Character count: ${inputLength}`;
 });
+
+for (let i = 0; i < 10; i++) {
+    const span = document.createElement('span');
+    span.textContent = textToAdd;
+    loopContainer.appendChild(span);
+}
 
 console.log('You got this! Do not give up!');
